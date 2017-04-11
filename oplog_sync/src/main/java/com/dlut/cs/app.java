84747111e12e816785783bb2df7f0c85f488f41e
刘首wei <you@example.com>
2017-04-11 23:06:13 +0800

    update Appi

--- oplog_sync/src/main/java/com/dlut/cs/app.java
+++ oplog_sync/src/main/java/com/dlut/cs/app.java
@@ -1,7 +1,9 @@
 package com.dlut.cs;
 
+import com.dlut.cs.client.Consumer;
 import com.dlut.cs.client.OplogScannerServer;
 import com.dlut.cs.client.Scanner;
+import com.dlut.cs.client.ShutdownHook;
 import com.mongodb.DBObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,8 +19,13 @@ public class app {
     private static final Logger logger = LoggerFactory.getLogger(app.class);
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("/Users/john_liu/graduation_design/oplog_sync/src/main/resources/META-INF/spring_config.xml");
-        OplogScannerServer server     = context.getBean(OplogScannerServer.class);
-        Scanner            scanner    = context.getBean(Scanner.class);
-        BlockingQueue<DBObject> queue = context.getBean(BlockingQueue.class);
+        OplogScannerServer      server     = context.getBean(OplogScannerServer.class);
+        Scanner                 scanner    = context.getBean(Scanner.class);
+        Consumer                consumer   = context.getBean(Consumer.class);
+        BlockingQueue<DBObject> queue      = context.getBean(BlockingQueue.class);
+        consumer.setQueue(queue);
+        scanner.setQueue(queue);
+        Runtime.getRuntime().addShutdownHook(new Thread(new ShutdownHook(server,scanner,consumer,queue)));
+        server.start();
     }
 }
