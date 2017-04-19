15841a79374cea51909f6073bbf9df043ffad2de
刘首wei <you@example.com>
2017-04-19 18:11:11 +0800

    update

--- oplog_sync/src/main/java/com/dlut/cs/client/Scanner.java
+++ oplog_sync/src/main/java/com/dlut/cs/client/Scanner.java
@@ -26,6 +26,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import com.alibaba.fastjson.JSONObject;
