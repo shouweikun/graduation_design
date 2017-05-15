0a54d07d6f584de5182376f1ca4db26265329e60
刘首wei <you@example.com>
2017-05-16 00:45:37 +0800

    update_code_still_need_test

--- flink_caculation/src/main/scala/datastream/AgeCount.scala
+++ flink_caculation/src/main/scala/datastream/AgeCount.scala
@@ -7,7 +7,7 @@ import java.util.Properties
 import org.apache.flink.api.common.functions.RuntimeContext
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.functions.sink.SinkFunction
-import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.connectors.elasticsearch2.{ElasticsearchSink, ElasticsearchSinkFunction, RequestIndexer}
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer08
@@ -16,9 +16,10 @@ import org.elasticsearch.client.Requests
 import utils.KafkaStringSchema
 
 /**
-  * Created by john_liu on 2017/5/10.
+  * Created by john_liu on 2017/5/15.
   */
-object GenderCount {
+object WindowAgeCount {
+
   def main(args: Array[String]): Unit = {
     //flink 环境
     val env = StreamExecutionEnvironment.getExecutionEnvironment
@@ -32,13 +33,15 @@ object GenderCount {
     //建立kafka consumer链接
     val kafkaConsumer = new FlinkKafkaConsumer08[String](
       "grau_info", KafkaStringSchema, prop)
+    //生成数据流
     val datastream = env.addSource(kafkaConsumer)
-     val resultStream = datastream.map{(_,1)}.keyBy(0).timeWindow(Time.seconds(500)).sum(1).map(x=>(x._1,x._2.toString))
+
+    //mapreduce
+    val resultStream  = datastream.map{x =>(division(x),1)}.keyBy(0).timeWindow(Time.seconds(500)).sum(1).map{x=>(x._1.toString,x._2.toString)}
 
 
-
-   //ElasticSearch配置
-   val config: java.util.Map[String,String] = new java.util.HashMap[String, String]
+    //ElasticSearch配置
+    val config: java.util.Map[String,String] = new java.util.HashMap[String, String]
     config.put("bulk.flush.max.actions", "1")
     config.put("cluster.name", "elasticsearch")
 
@@ -55,16 +58,26 @@ object GenderCount {
         }
         Requests.indexRequest.index("grau").`type`("window_age_count_result").source(map)
       }
-      def process(element: (String,String), runtimeContext: RuntimeContext, requestIndexer: RequestIndexer): Unit = requestIndexer.add(createIndexRequest(element))
+       def process(element: (String,String), runtimeContext: RuntimeContext, requestIndexer: RequestIndexer): Unit = requestIndexer.add(createIndexRequest(element))
     }
     val essink  = new ElasticsearchSink(config,transports,elasticsearchSink) with SinkFunction[(String,String)]{}
 
     resultStream.addSink(essink)
-    }
-
-  def mark(x:String) ={
-    if(x.trim.equals("F")) ("0")
-    else ("1")
   }
-
+ def division(x:String) ={
+   (x.toInt)/10.toInt match {
+     case 0 => 0
+     case 1 => 1
+     case 2 => 2
+     case 3 => 3
+     case 4 => 4
+     case 5 => 5
+     case 6 => 6
+     case 7 => 7
+     case 8 => 8
+     case 9 => 9
+     case int:Int =>10
+     case any => throw new Exception
+   }
+ }
 }
--- flink_caculation/src/main/scala/datastream/Agecount.scala
+++
@@ -1,8 +0,0 @@
-package datastream
-
-/**
-  * Created by john_liu on 2017/5/15.
-  */
-object AgeCount {
-
-}
